95c07d0495d992ab930b3f13595120a46ffba287
joakimtj <joakimtj@hiof.no>
2024-09-05 18:55:34 +0200

    This is a total mess and I can't be bothered to sort this out. I will do better next time.

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
+import { cors } from "hono/cors";
+import fs from "node:fs/promises";
+import { serveStatic } from "@hono/node-server/serve-static";
+const app = new Hono();
+app.use("/*", cors());
+app.use("/*", serveStatic({ root: "./" }));
+const projects = [];
+// Endpoint to retrieve all projects
+app.get('/projects', async (c) => {
+    const data = await fs.readFile("./projects.json", "utf8");
+    const dataAsJson = JSON.parse(data);
+    projects.push(...dataAsJson);
+    console.log(projects);
+});
+// Start the server
+serve(app);
