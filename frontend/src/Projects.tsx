df258f9fd9fdef9c8968c8a8789610a4018b1484
joakimtj <joakimtj@hiof.no>
2024-09-27 16:16:40 +0200

    add bandaid technologies count calc

--- frontend/src/Projects.tsx
+++ frontend/src/Projects.tsx
@@ -1,18 +1,64 @@
+import { useMemo } from "react";
 import Project from "./Project"
+
+interface tech {
+    name: string,
+    count: number
+}
+
+interface techs {
+    data: tech[] | null,
+}
 
 export default function Projects({ projects }: any) {
     if (!projects || projects.length === 0) {
         projects = [{ id: 0, title: "Error", description: "ERROR: No projects.", technologies: [], date: 0 }]
     }
 
-    return (
-        <section id="projects-section">
-            {projects.map((project: any) =>
-                <Project key={project.id} id={project.id} title={project.title} description={project.description}
-                    technologies={project.technologies} date={project.date}>
+    const alltechs: techs = useMemo(() => {
+        if (!projects || projects.length === 0) {
+            return { data: null };
+        }
 
-                </Project>
-            )}
-        </section>
+        const techs: techs = { data: null };
+
+        for (let p of projects) {
+            for (let t of p.technologies) {
+                if (!techs.data) {
+                    techs.data = [{ name: t, count: 1 }];
+                } else {
+                    const itemIndex = techs.data.findIndex((e) => e.name === t);
+                    if (itemIndex === -1) {
+                        techs.data = [...techs.data, { name: t, count: 1 }];
+                    } else {
+                        techs.data[itemIndex].count += 1;
+                    }
+                }
+            }
+        }
+
+        return techs;
+    }, [projects]);
+
+    if (!projects || projects.length === 0) {
+        projects = [{ id: 0, title: "Error", description: "ERROR: No projects.", technologies: [], date: 0 }];
+    }
+
+    console.log(alltechs.data);
+    return (
+        <>
+            <section id="projects-section">
+                {projects.map((project: any) =>
+                    <Project key={project.id} id={project.id} title={project.title} description={project.description}
+                        technologies={project.technologies} date={project.date}>
+
+                    </Project>
+                )}
+            </section>
+            <ul>
+                {alltechs.data?.map((t) =>
+                    <li key={t.name}>{t.name}: {t.count}</li>)}
+            </ul>
+        </>
     )
 }
